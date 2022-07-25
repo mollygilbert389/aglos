b8ae06b91fa46d4e49a951595e8f78c382667230
Molly Gilbert <mgilbert@atlassian.com>
2022-07-25 11:37:42 -0500

    adding not great solution to smallest difference

--- UT-Algos/39-smallest-difference/smallestDifference.js
+++ UT-Algos/39-smallest-difference/smallestDifference.js
@@ -1,11 +1,21 @@
 function smallestDifferenceFunc(arr1, arr2) {
     const finalArr = []
     
-    arr1.forEach((item) => {
-        arr2.forEach((num) => {
-            item 
+    arr1.forEach((firstArrNum, idx) => {
+        arr2.forEach((secondArrNum, index) => {
+            const difference = Math.abs(firstArrNum - secondArrNum);
+            finalArr.push({
+                difference, 
+                firstArrIndex: idx,
+                secondArrIndex: index
+            })
         })
     })
+
+    const sortedArr = finalArr.sort((a, b) => a.difference - b.difference)
+    const foundObj = sortedArr[0]
+
+    return [arr1[foundObj.firstArrIndex], arr2[foundObj.secondArrIndex]]
 }
 
 module.exports = { smallestDifferenceFunc }
